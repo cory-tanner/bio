9deb720b739a980d005bc6a4ce9b166cedf37fea
Cory Tanner <cory.tanner@dockyard.com>
2019-11-02 15:42:01 -0600

    Update with new about me copy

--- src/components/modules/sectionAboutMe.js
+++ src/components/modules/sectionAboutMe.js
@@ -6,8 +6,8 @@ import '../../styles/components/about-me.css';
 const SectionAboutMe = () => (
   <section className="about-me">
     <h3 className="t-section-heading about-me__heading">What I Do</h3>
-    <p className="t-body about-me__copy">Hi! I’m a Senior User Experience Developer that focuses/specializes on the usability of applications. I believe that the User Experience and User Interaction are key aspects of an application that are overlooked at times.</p>
-    <p className="t-body about-me__copy">I aim to bring UX/UI to the forefront and develop for the user. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
+    <p className="t-body about-me__copy">Hi! I’m a Senior User Experience Developer that specializes on the usability of applications. I believe that the User Experience is a key aspects of an applications success.</p>
+    <p className="t-body about-me__copy">I aim to bring UX/UI to the forefront of app development and develop for the user. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
   </section>
 )
 
