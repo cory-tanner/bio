37691de68a7b81f76cc61cdba9f35b069caa506f
Cory Tanner <cory.tanner@dockyard.com>
2019-12-03 10:53:20 -0700

    Hotfix for intro copy

--- src/components/modules/sectionAboutMe.js
+++ src/components/modules/sectionAboutMe.js
@@ -6,8 +6,8 @@ import '../../styles/components/about-me.css';
 const SectionAboutMe = () => (
   <section className="about-me">
     <h3 className="t-section-heading about-me__heading">What I Do</h3>
-    <p className="t-body about-me__copy">Hi! I’m a Senior User Experience Developer that specializes on the usability of applications. I believe that the User Experience is a key aspects of an applications success.</p>
-    <p className="t-body about-me__copy">I aim to bring UX/UI to the forefront of app development and develop for the user. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
+    <p className="t-body about-me__copy">Hi! I’m a Senior User Experience Developer that focuses/specializes on the usability of applications. I believe that the user experience of an application should impact the development process and practices we as developers use in our daily routines.</p>
+    <p className="t-body about-me__copy">I aim to always develop for the user and do what is best for their experience on the web. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
   </section>
 )
 
