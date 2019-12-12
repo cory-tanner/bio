ef75b79ee200aead1b924027a0f9692c53d4bf0b
Cory Tanner <cory.tanner@dockyard.com>
2019-12-12 14:25:48 -0700

    Add react-ga

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './App.css';
 import Hero from './components/hero';
 import Main from './components/main';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-154481477-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
