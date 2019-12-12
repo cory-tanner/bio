1881378b92673bc83bb7ab5c248ac4b2a01b565f
Cory Tanner <ctannerweb@gmail.com>
2019-12-12 14:45:11 -0700

    Hotfix for react-ga (#19)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from './components/main';
 import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-154481477-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.pageview("/index");
 
 function App() {
   return (
