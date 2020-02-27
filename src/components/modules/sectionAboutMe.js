2120862be912ef1e4d136678fca0639229d2c692
Cory Tanner <cory.tanner@dockyard.com>
2020-02-27 14:47:23 -0700

    Use 12 column grid

--- src/components/modules/sectionAboutMe.js
+++ src/components/modules/sectionAboutMe.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const SectionAboutMe = () => (
-  <section className="about-me">
-    <h3 className="t-section-heading about-me__heading">What I Do</h3>
-    <p className="t-body about-me__copy">Hi! I’m a Senior User Experience Developer that focuses/specializes on the usability of applications. I believe that the user experience of an application should impact the development process and practices we as developers use in our daily routines.</p>
-    <p className="t-body about-me__copy">I aim to always develop for the user and do what is best for their experience on the web. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
+  <section className="about-me grid">
+    <h3 className="t-section-heading about-me__heading grid__item">What I Do</h3>
+    <p className="t-body about-me__copy grid__item" data-grid-large="6">Hi! I’m a Senior User Experience Developer that focuses/specializes on the usability of applications. I believe that the user experience of an application should impact the development process and practices we as developers use in our daily routines.</p>
+    <p className="t-body about-me__copy grid__item" data-grid-large="6">I aim to always develop for the user and do what is best for their experience on the web. All while developing with the latest tools and keeping a simplistic/modern approach.</p>
   </section>
 )
 
--- src/components/modules/sectionProjects.js
+++ src/components/modules/sectionProjects.js
@@ -6,7 +6,7 @@ class SectionProjects extends Component {
   render () {
     return (
       <section className="projects grid">
-        <h3 className="t-section-heading projects__heading grid__item" data-grid-small="12">Side Projects</h3>
+        <h3 className="t-section-heading projects__heading grid__item">Side Projects</h3>
 
         {this.props.projects.map((projects) =>
           <SideProject key={projects.id} projects={projects} />
