701d1117806f7a31dded4144bad24ef2ef0c5888
Cory Tanner <ctannerweb@gmail.com>
2020-03-10 16:21:59 -0600

    Correct blog title

--- src/components/main.js
+++ src/components/main.js
@@ -10,14 +10,14 @@ class Main extends Component {
       {
         id: 1,
         link: 'https://dockyard.com/blog/2019/12/05/know-the-code-how-design-and-engineering-team-up-for-design-qa',
-        title: 'narwin-grid: Harmony Between CSS Grid and Data Attributes',
+        title: 'Know the Code: How Design and Engineering Team Up for Design QA',
         dateTime: '2019-04-17T16:20:44.288Z',
         date: 'December 5, 2019'
       },
       {
         id: 2,
         link: 'https://dockyard.com/blog/2019/04/17/narwin-grid-harmony-between-css-grid-and-data-attributes',
-        title: 'Know the Code: How Design and Engineering Team Up for Design QA',
+        title: 'narwin-grid: Harmony Between CSS Grid and Data Attributes',
         dateTime: '2019-12-05T18:03:08.082Z',
         date: 'April 17, 2019'
       },
