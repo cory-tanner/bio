d879ba8fdb7b8b958bda081589a375f9a192a717
Cory Tanner <ctannerweb@gmail.com>
2020-03-13 17:22:30 -0600

    Add onClick event on mountains (#32)

--- src/components/modules/mountainsSvg.js
+++ src/components/modules/mountainsSvg.js
@@ -1,45 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MountainsSvg = () => (
-  <div className="mountain-svg">
-    <svg className="mountain-svg__first" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
-      <g className="mountain-svg__flag">
-        <path d="m23 12h10v6h-10" fill="var(--color-link)"/>
-        <path d="m23 12v3.861c.482.088.983.139 1.5.139 3.145 0 5.767-1.718 6.369-4z" fill="var(--color-brand-2)"/>
-      </g>
-      <path d="m22 9h2v20h-2z" fill="#8892a0"/>
-      <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
-      <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
-      <path d="m48 49 4.457-3.343-4.457-9.657-4.457 9.657z" fill="var(--color-gray-2)"/>
-      <path d="m48 36-4.457 9.657 1.329.997c2.527-.886 4.522-3.101 5.319-5.908z" fill="#ebf7fe"/>
-      <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
-      <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
-      <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
-      <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
-      <path className="mountain-svg__bounce-3" d="m58 14c2.209 0 4-1.791 4-4s-1.791-4-4-4c-.75 0-1.443.219-2.043.578-.217-2.561-2.34-4.578-4.957-4.578-2.761 0-5 2.239-5 5 0 .43.071.841.173 1.239-.36-.154-.756-.239-1.173-.239-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
-      <path className="mountain-svg__bounce-4" d="m15 14c1.657 0 3-1.343 3-3s-1.343-3-3-3c-.035 0-.068.009-.102.01.067-.326.102-.664.102-1.01 0-2.761-2.239-5-5-5s-5 2.239-5 5c0 .346.035.684.102 1.01-.034-.001-.067-.01-.102-.01-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
-    </svg>
-    <svg className="mountain-svg__second" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
-      <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
-      <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
-      <path d="m48 49 4.457-3.343-4.457-9.657-4.457 9.657z" fill="var(--color-gray-2)"/>
-      <path d="m48 36-4.457 9.657 1.329.997c2.527-.886 4.522-3.101 5.319-5.908z" fill="#ebf7fe"/>
-      <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
-      <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
-      <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
-      <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
-      <path className="mountain-svg__bounce" d="m58 14c2.209 0 4-1.791 4-4s-1.791-4-4-4c-.75 0-1.443.219-2.043.578-.217-2.561-2.34-4.578-4.957-4.578-2.761 0-5 2.239-5 5 0 .43.071.841.173 1.239-.36-.154-.756-.239-1.173-.239-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
-      <path className="mountain-svg__bounce-2" d="m15 14c1.657 0 3-1.343 3-3s-1.343-3-3-3c-.035 0-.068.009-.102.01.067-.326.102-.664.102-1.01 0-2.761-2.239-5-5-5s-5 2.239-5 5c0 .346.035.684.102 1.01-.034-.001-.067-.01-.102-.01-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
-    </svg>
-    <svg className="mountain-svg__third" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
-      <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
-      <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
-      <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
-      <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
-      <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
-      <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
-    </svg>
-  </div>
-)
+class MountainsSvg extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cloudState: false
+    }
+    
+    this.handleCloudState = this.handleCloudState.bind(this)
+  }
+
+  handleCloudState = () => {
+    this.setState((currentState) => ({
+      cloudState: !currentState.cloudState
+    }))
+  }
+
+  render () {
+    const {cloudState} = this.state;
+
+    return (
+      <div className="mountain-svg" role="presentation" onClick={this.handleCloudState} data-cloud-state={cloudState ? "clear" : "cloudy"}>
+        <svg className="mountain-svg__first" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
+          <path className="mountain-svg__bounce-3" d="m58 14c2.209 0 4-1.791 4-4s-1.791-4-4-4c-.75 0-1.443.219-2.043.578-.217-2.561-2.34-4.578-4.957-4.578-2.761 0-5 2.239-5 5 0 .43.071.841.173 1.239-.36-.154-.756-.239-1.173-.239-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
+          <path className="mountain-svg__bounce-4 mountain-svg__sun-cloud" d="m15 14c1.657 0 3-1.343 3-3s-1.343-3-3-3c-.035 0-.068.009-.102.01.067-.326.102-.664.102-1.01 0-2.761-2.239-5-5-5s-5 2.239-5 5c0 .346.035.684.102 1.01-.034-.001-.067-.01-.102-.01-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
+          <g className="mountain-svg__flag">
+            <path d="m23 12h10v6h-10" fill="var(--color-link)"/>
+            <path d="m23 12v3.861c.482.088.983.139 1.5.139 3.145 0 5.767-1.718 6.369-4z" fill="var(--color-brand-2)"/>
+          </g>
+          <path d="m22 9h2v20h-2z" fill="#8892a0"/>
+          <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
+          <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
+          <path d="m48 49 4.457-3.343-4.457-9.657-4.457 9.657z" fill="var(--color-gray-2)"/>
+          <path d="m48 36-4.457 9.657 1.329.997c2.527-.886 4.522-3.101 5.319-5.908z" fill="#ebf7fe"/>
+          <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
+          <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
+          <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
+          <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
+          <circle className="mountain-svg__sun" cx="8" cy="8" r="8" fill="var(--color-sun)"/>
+        </svg>
+        <svg className="mountain-svg__second" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
+          <path className="mountain-svg__bounce" d="m58 14c2.209 0 4-1.791 4-4s-1.791-4-4-4c-.75 0-1.443.219-2.043.578-.217-2.561-2.34-4.578-4.957-4.578-2.761 0-5 2.239-5 5 0 .43.071.841.173 1.239-.36-.154-.756-.239-1.173-.239-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
+          <path className="mountain-svg__bounce-2" d="m15 14c1.657 0 3-1.343 3-3s-1.343-3-3-3c-.035 0-.068.009-.102.01.067-.326.102-.664.102-1.01 0-2.761-2.239-5-5-5s-5 2.239-5 5c0 .346.035.684.102 1.01-.034-.001-.067-.01-.102-.01-1.657 0-3 1.343-3 3s1.343 3 3 3z" fill="var(--color-gray-1)"/>
+          <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
+          <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
+          <path d="m48 49 4.457-3.343-4.457-9.657-4.457 9.657z" fill="var(--color-gray-2)"/>
+          <path d="m48 36-4.457 9.657 1.329.997c2.527-.886 4.522-3.101 5.319-5.908z" fill="#ebf7fe"/>
+          <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
+          <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
+          <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
+          <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
+        </svg>
+        <svg className="mountain-svg__third" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" role="presentation">
+          <path d="m36 62 12-26 12 26" fill="var(--color-brand-3)"/>
+          <path d="m47.845 36.335-11.845 25.665h6.444c4.806-2.658 8.159-8.593 8.159-15.502 0-3.816-1.034-7.326-2.758-10.163z" fill="var(--color-brand-2)"/>
+          <path d="m42 62-19-38-19 38" fill="var(--color-brand-3)"/>
+          <path d="m23 24-16.993 33.985c.165.004.328.015.493.015 11.137 0 20.458-9.069 22.881-21.239z" fill="var(--color-brand-2)"/>
+          <path d="m17 36 6 4 6-4-6-12z" fill="var(--color-gray-2)"/>
+          <path d="m23 24-5.119 10.238c.644.484 1.36.762 2.119.762 2.565 0 4.676-3.092 4.964-7.073z" fill="#ebf7fe"/>
+        </svg>
+      </div>
+    )
+  }
+}
 
 export default MountainsSvg;
