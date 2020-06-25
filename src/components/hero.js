97044466ed9107c9a7b3d236d16fefea2f079397
Cory Tanner <ctannerweb@gmail.com>
2020-06-25 14:54:30 -0600

    Update job title

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ const Hero = () => (
   <header className="hero">
     <div className="hero__headings">
       <h1 className="t-display hero__headings__display">Cory Tanner</h1>
-      <h2 className="t-sub-display hero__headings__sub-display">Senior UX Developer</h2>
+      <h2 className="t-sub-display hero__headings__sub-display">Senior UI Engineer</h2>
     </div>
     <Socials />
     <MountainsSvg />
